9bef49111394cbf012549a31c19b5e9096248150
Matthew Pollicito <mpollicito@yahoo.com>
2020-09-06 18:46:25 -0400

    Second commit

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # homework5
 Work Day Scheduler
-
+----------------------------------------------------------------------------------------------------------------------------------
 Hello! This app is used for planning out your work day using "Moment" JavaScript along with jQuery.
 You can input an activity into each hour slot (9am-4pm) and submit
-your input with a save button (uses local storage). 
+your input with a save button (uses local storage).
+
+----------------------------------------------------------------------------------------------------------------------------------
 This assignment uses jQuery to dynamically place time blocks onto the webpage and allow users to input text and save it.
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(function () {
   alert("Good day!");
   
-  $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+  $("#currentDay").text(moment().format('MMMM Do, YYYY'));
     
   for (i = 9; i < 17; i++) {
     // if i is less than current hour, provide a class to indicate a current, present or past event
     // $(".container").append("");
     var newTimeBlock = $("<div id='"+ i +"' data-hour='" + i + "'>" + i + "</div>");
 
-    newTimeBlock.append("<button type='button' class='btn btn-secondary'>"+ i +"</button>");
+    newTimeBlock.append("<button type='button' class='btn btn-secondary'>" + i + "</button>");
     // look up providing default value for inputs
     newTimeBlock.append("<input/>");
     newTimeBlock.append("<button class='submitText'>Save<button/>");
@@ -19,5 +19,25 @@ $(function () {
   }
   
   //  find how to select different elements of the same class jQuery
+
+
   // week 5 activity 11 line#97
+  // $(".submit-text").on("click", function() {
+
+  //   var fridgeMagnet = $("<div>");
+
+  //   fridgeMagnet.addClass("letter fridge-color");
+
+  //   fridgeMagnet.text($(this).attr("data-letter"));
+
+  // });
+
+
+  // $("#clear").on("click", function() {
+  //   $("#display").empty();
+
+  // });
+
+
+
 });
