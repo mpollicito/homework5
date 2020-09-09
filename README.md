eb2a4ad5de7c6d8e84ed120d6fc56daefa10ea2c
Matthew Pollicito <mpollicito@yahoo.com>
2020-09-08 21:39:25 -0400

    5th commit, trying to rework the app

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # homework5
 Work Day Scheduler
 ----------------------------------------------------------------------------------------------------------------------------------
-Hello! This app is used for planning out your work day using "Moment" JavaScript along with jQuery.
+Hello! This assignment was to create an app for planning out your work day using "Moment" JavaScript along with jQuery to do dynamicals 
 You can input an activity into each hour slot (9am-4pm) and submit
 your input with a save button (uses local storage).
 
 ----------------------------------------------------------------------------------------------------------------------------------
-This assignment uses jQuery to dynamically place time blocks onto the webpage and allow users to input text and save it.
--- script.js
+++ script.js
@@ -2,53 +2,13 @@ $(function () {
   alert("Good day!");
   
   $("#currentDay").text(moment().format('MMMM Do, YYYY'));
-    
-  for (i = 9; i < 17; i++) {
-    // if i is less than current hour, provide a class to indicate a current, present or past event
-    // $(".container").append("");
-    var newTimeBlock = $("<div id='"+ i +"' data-hour='" + i + "'>" + i + "</div>");
 
-    // newTimeBlock.append("<button type='button' class='btn btn-secondary'>" + i + "</button>");
-    // look up providing default value for inputs
-    newTimeBlock.append("<input/>");
-    newTimeBlock.append("<button class='saveBtn'>Save<button/>");
-    //  add localStorage(); functionality
+
+  saveButton.on("click", function () {
+    var rowHourLS = $(this).attr("data-hour");
+    var input = $("#" + rowHourLS + "Row").val();
+    localStorage.setItem(rowHourLS, input);
 
     $(".container").append(newTimeBlock);
-
-    if (newTimeBlock = true ) {
-      $(".saveBtn").on("click", function() {
-
-        var submitText = $();
-        
-        submitText.addClass();
-        
-        });
-    } else {
-      console.log(submitText);
-    }
-
-  }
-  //  find how to select different elements of the same class jQuery
-
-
-  // week 5 activity 11 line#97
-  // $(".submit-text").on("click", function() {
-
-  //   var fridgeMagnet = $("<div>");
-
-  //   fridgeMagnet.addClass("letter fridge-color");
-
-  //   fridgeMagnet.text($(this).attr("data-letter"));
-
-  // });
-
-
-  // $("#clear").on("click", function() {
-  //   $("#display").empty();
-
-  // });
-
-
-
+  });
 });
